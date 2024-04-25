03d49f1783beb326fa0da5627645105003d37403
Gerardo vasquez <gvsvasquez90@gmail.com>
2024-04-25 17:01:11 -0600

    formulario

---
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+interface MenuItem{
+  title:string;
+  route:string;
+}
+
+@Component({
+  selector: 'shared-side-menu',
+  templateUrl: './side-menu.component.html',
+ 
+})
+export class SideMenuComponent {
+
+  public reactiveMenu:MenuItem[]=[
+    { title:'Básicos',route:'./reactive/basic'},
+    { title:'Dimanicos',route:'./reactive/dynamic'},
+    { title:'Switches',route:'./reactive/switches'},
+   ];
+
+   public authMenu:MenuItem[]=[
+    { title:'Registro',route:'./auth'},
+   
+   ];
+
+
+
+}
